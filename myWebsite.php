c10d37c6a5568585dbf0db392c02e79a36c97676
shiras-shira <63020094+shiras-shira@users.noreply.github.com>
2025-10-11 21:52:40 -0400

    php2

--- myWebsite.php
+++ myWebsite.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php //embedding php in html code ?>
+    <p> This is an <?php echo "AWESOME" ?> paragraph!</p>
+    <?php
+        /* multiple
+        line
+        comment! */
+        //outputting hello world to browser
+        echo "This is ALSO a paragraph!";
+        //var with $
+        $initials = "SNK";
+        echo $initials;
+    ?>
+</body>
+</html>
