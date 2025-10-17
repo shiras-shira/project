efa94e6c30c9a9e0a3dc145fec14f5f1e37081ed
shiras-shira <63020094+shiras-shira@users.noreply.github.com>
2025-10-16 20:02:12 -0400

    did a few yt vids

--- myWebsite.php
+++ myWebsite.php
@@ -6,17 +6,39 @@
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    //scalar types = contains one value
+    $hi = "Hello World!";
+    $adjective = "AWESOME";
+    //array types = contains multiple values
+    $fruits = array("Apple", "Banana", "Cherry");
+    $fruits2 = ["Apple", "Banana", "Cherry"];
+    ?>
     <?php //embedding php in html code ?>
-    <p> This is an <?php echo "AWESOME" ?> paragraph!</p>
+    <p> This is an <?php echo $adjective; ?> paragraph!</p>
     <?php
         /* multiple
         line
         comment! */
         //outputting hello world to browser
-        echo "This is ALSO a paragraph!";
+        echo "This is ALSO a paragraph! <br>";
         //var with $
-        $initials = "SNK";
+        $initials = "SNK <br>";
         echo $initials;
+
+        //pre-defined variables
+        echo "Document Root: ";
+        echo $_SERVER["DOCUMENT_ROOT"];
+        echo "<br>";
+        echo "Current Page: ";
+        echo $_SERVER['PHP_SELF'];
+        echo "<br>";
+        echo "Server Name: ";
+        echo $_SERVER['SERVER_NAME'];
+        echo "<br>";
+        echo $_GET["name"];
+        //request method grabs everything
+        echo $_REQUEST["name"];
     ?>
 </body>
 </html>
